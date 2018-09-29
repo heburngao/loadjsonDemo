a1d429c7211802e99ec26c9358e24e6660dbe266
heburngao <heburngao@localhost.localdomain>
2018-09-29 23:42:45 +0800

    modify

--- testload.go
+++ testload.go
@@ -16,11 +16,11 @@ type Config struct {
 	Name string 
 	Order string
 }
-
 var (
 	configs []Config
+	//dataMarshal []Config
 )
-func loadconfigs() bool {
+func loadConfig() bool {
 //=============load json
 	buff , err := ioutil.ReadFile("jsn.json")
 	if err != nil {
@@ -36,32 +36,29 @@ func loadconfigs() bool {
 	fmt.Println(" the json is : " , string(buff))
 //================Unmarshal json
 	err = json.Unmarshal(buff , & configs) //  read buff >>> configs
-	
 	if err != nil {
 		fmt.Println("parse json error : " , err)
 		return false
 	}
 	fmt.Printf("configs struct : %+v \r\n" , configs)
 	fmt.Printf("configs[0].Name : %+v \r\n" , configs[0].Name)
-
-
 //=============== Marshal json
-	var p []Config //= make(*[]Config) 
+	var p []Config = make([]Config, 0) 
 	ooxx := Config{
 			Name : "oo" , 
 			Order : "xx" ,
 		}
 	p = append(p,ooxx)
-	
+	ooxx.Name = "nooo"
+	p = append(p , ooxx)
 	str , _ := json.Marshal(p)
 	fmt.Printf("json is :%s \r\n" , string(str))
 	return true
 }
 func main(){
 	fmt.Println("main")
-	loadconfigs()
+	loadConfig()
 	//fmt.Println("load configs , mode: " , configs.Mode)
-
 }
 func init(){
 	fmt.Println("init")
