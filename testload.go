19feea7374d1a5e318c0c25168d42d30499e4768
heburngao <heburngao@localhost.localdomain>
2018-09-29 21:47:40 +0800

    go jsn marshel unmarshel

---
+++ testload.go
@@ -0,0 +1,68 @@
+package main 
+
+import (
+"fmt"
+"encoding/json"
+"io/ioutil"
+
+)
+/*
+type Config struct {
+	Mode string `json:"Mode"`
+}
+*/
+
+type Config struct {
+	Name string 
+	Order string
+}
+
+var (
+	configs []Config
+)
+func loadconfigs() bool {
+//=============load json
+	buff , err := ioutil.ReadFile("jsn.json")
+	if err != nil {
+		fmt.Println("load json error : " , err)
+		return false
+	}
+	/*
+	var buff = [ ] byte ( ` [ 
+        	{ "Name" : "Platypus" , "Order" : "Monotremata" } , 
+	        { "Name" : "Quoll" ,     "Order" : "Dasyuromorphia" } 
+    	] ` ) 
+	*/
+	fmt.Println(" the json is : " , string(buff))
+//================Unmarshal json
+	err = json.Unmarshal(buff , & configs) //  read buff >>> configs
+	
+	if err != nil {
+		fmt.Println("parse json error : " , err)
+		return false
+	}
+	fmt.Printf("configs struct : %+v \r\n" , configs)
+	fmt.Printf("configs[0].Name : %+v \r\n" , configs[0].Name)
+
+
+//=============== Marshal json
+	var p []Config //= make(*[]Config) 
+	ooxx := Config{
+			Name : "oo" , 
+			Order : "xx" ,
+		}
+	p = append(p,ooxx)
+	
+	str , _ := json.Marshal(p)
+	fmt.Printf("json is :%s \r\n" , string(str))
+	return true
+}
+func main(){
+	fmt.Println("main")
+	loadconfigs()
+	//fmt.Println("load configs , mode: " , configs.Mode)
+
+}
+func init(){
+	fmt.Println("init")
+}
